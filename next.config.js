8d64961ea2a9d9748f6231db69c84b60d3297147
“halmuda” <halmuda6231@gmail.com>
2021-09-02 22:13:26 +0900

    update: #4 api呼び出し方修正

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    gasApiEndPoint: 'https://script.google.com/macros/s/AKfycbwhodlUv89ymI_fQ7Umkw5Z4JFa2ySYMNqwW0QyhDHybwU8r6RiZxgZ6YyIK_-FFGpE2w/exec'
+    gasApiEndPoint: 'https://script.google.com/macros/s/AKfycbzU4_jHD5f7mCOHe7NEzJRfQt7b56nzKOmLu8qh8zox68znpLlx2OxpC2IdzKCThRnUUQ/exec'
   }
 };
