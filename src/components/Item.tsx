9deca15ce43578b6946286a96f8deb9e278e41bd
“halmuda” <halmuda6231@gmail.com>
2023-04-30 14:36:03 +0900

    編集ボタンからモーダルを開くようにする

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -17,15 +17,25 @@ const customStyles = {
 }
 
 export default function Item({ item, type, onCheck, onInput }) {
+  const [formData, setFormData] = useState(Object.assign({}, item))
   const handleCheck = () => {
-    // onCheck(item, type);
-    setEditModalIsOpen(true)
-  }
-  const handleInput = (event: any) => {
-    item.name = event.target.value
-    onInput(item, type)
+    onCheck(item, type)
   }
 
+  const handleInput = (event: any) => {
+    formData.name = event.target.value
+    setFormData(formData)
+  }
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    item.name = formData.name
+    item.icon = formData.icon
+    onInput(item, type)
+    setEditModalIsOpen(false)
+  }
+  const openModal = () => {
+    setEditModalIsOpen(true)
+  }
   const closeModal = () => {
     setEditModalIsOpen(false)
   }
@@ -33,25 +43,32 @@ export default function Item({ item, type, onCheck, onInput }) {
   const [editModalIsOpen, setEditModalIsOpen] = useState(false)
 
   return (
-    <label className={styles.card}>
-      <input type='checkbox' checked={item.purchased} onChange={handleCheck} />
-      <span className={item.purchased ? 'done-item' : ''}>{item.name}</span>
-      <span>{item.icon}</span>
+    <div className={styles.card}>
+      <label>
+        <input type='checkbox' checked={item.purchased} onChange={handleCheck} />
+        <span className={item.purchased ? 'done-item' : ''}>{item.name}</span>
+        <span>{item.icon}</span>
+      </label>
+      <span className={styles.editIcon} onClick={openModal}>
+        ✏️
+      </span>
       <Modal isOpen={editModalIsOpen} style={customStyles}>
-        <input
-          className={item.purchased ? 'done-item' : ''}
-          type='text'
-          defaultValue={item.name}
-          onInput={handleInput}
-        />
-        {/* {TODO: あとで足す} */}
-        <Button variant='contained' color='primary' onClick={handleCheck}>
-          change
-        </Button>
-        <Button variant='contained' color='primary' onClick={closeModal}>
-          close
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={item.purchased ? 'done-item' : ''}
+            type='text'
+            defaultValue={item.name}
+            onInput={handleInput}
+          />
+          <input type='text' defaultValue={item.icon} />
+          <Button type='submit' variant='contained' color='primary'>
+            change
+          </Button>
+          <Button variant='contained' color='primary' onClick={closeModal}>
+            close
+          </Button>
+        </form>
       </Modal>
-    </label>
+    </div>
   )
 }
